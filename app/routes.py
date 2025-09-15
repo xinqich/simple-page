938275f148b85a1be3fdce60049cfd13f71a9119
xinqich <timurcallo@gmail.com>
2025-09-15 19:17:16 +0300

    Create page structure and basic routing

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
 from flask import render_template
+
 from app import app
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    return render_template("contact.html")
