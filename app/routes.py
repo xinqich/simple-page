848a180cf06f61693b1a2a7a9a25a734c3e2fece
xinqich <timurcallo@gmail.com>
2025-09-20 15:31:25 +0300

    more information

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,45 @@
 from flask import redirect, render_template, request, url_for
+from datetime import datetime
 
 from app import app
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", time=datetime.now())
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    team_members = [
+        {'name': 'Alice', 'role': 'Developer'},
+        {'name': 'Bob', 'role': 'Designer'},
+        {'name': 'Charlie', 'role': 'Project Manager'}
+    ]
+    return render_template("about.html", team=team_members)
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    customer_care = {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031",
+        "website": "hildegard.org",
+    }
+    return render_template("contact.html", customer_care=customer_care)
 
 
 @app.route("/result", methods=["POST", "GET"])
